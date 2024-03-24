95799721f8b1e0d88dda253da37209a06616f843
Luke Reed <luke.reed@outlook.com.au>
2024-03-24 15:44:53 +1100

    Functions for: - Getting ttm cash flow - Getting estimated 5yr growth rate - Getting 4 year previous EPS value - Getting 4 year previous year prices

--- main.py
+++ main.py
@@ -23,16 +23,36 @@ def get_est_5yr_growth_rate(ticker):
 
 def get_prev_4_year_eps(ticker):
     company = yf.Ticker(ticker)
-    eps = company.income_stmt.loc["Diluted EPS"]
+    income_statement = company.income_stmt
+    prev_4_year_dates = income_statement.columns
+    prev_4_year_eps_values = income_statement.loc["Diluted EPS"]
 
-    prev_eps = []
+    prev_4_year_eps_map = {}
     for i in range(4):
-        prev_eps.append(eps.iloc[i])
+        prev_4_year_eps_map[str(prev_4_year_dates[i]-pd.Timedelta(days=1))[:10]] = prev_4_year_eps_values.iloc[i]
 
-    return prev_eps
+    return prev_4_year_eps_map
 
 
-def
+def get_prev_4_year_prices(ticker, prev_years):
+    company = yf.Ticker(ticker)
+    history = company.history(period="4y")
+
+    prev_4_year_prices_map = {}
+    for year in prev_years:
+        if year in history.index:
+            prev_4_year_prices_map[year] = round(history.loc[year].iloc[3], 2)
+
+    return prev_4_year_prices_map
+
 
 ticker = "AAPL"
-get_prev_4_year_eps(ticker)
+
+historic_eps = get_prev_4_year_eps(ticker)
+historic_prices = get_prev_4_year_prices(ticker, historic_eps.keys())
+
+for k, v in historic_prices.items():
+    print(k, v)
+
+for k, v in historic_eps.items():
+    print(k, v)
